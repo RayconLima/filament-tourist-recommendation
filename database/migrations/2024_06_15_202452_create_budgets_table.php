81851678dafba9cfd74d77a5cb9425de5b0dbeae
Raycon Lima Batista <rayconlimabatista@gmail.com>
2024-06-16 02:20:40 -0400

    feat: add Guide

--- database/migrations/2024_06_15_202452_create_budgets_table.php
+++ database/migrations/2024_06_15_202452_create_budgets_table.php
@@ -14,7 +14,7 @@ return new class extends Migration
         Schema::create('budgets', function (Blueprint $table) {
             $table->id();
             $table->foreignIdFor(\App\Models\User::class);
-            $table->double('budget_amount', 13, 2);
+            $table->float('budget_amount', 13, 2);
             $table->timestamps();
         });
     }
